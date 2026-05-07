e6f7456c9aaaa552309c1685b5855ea9a221aa55
Shomurotova Mashhura <shomurotovamashhura04@gmail.com>
2026-05-07 16:13:54 +0500

    ...

---
+++ function.py
@@ -0,0 +1,32 @@
+#  Pythonda function bilan tanishish
+
+# print("Assalomu alaykum")
+# a=34
+
+# print(type(a))
+
+# def salom_ber():
+#     print(f"Salom {ism} axvollaring yaxshimi")
+    
+# salom_ber("bobur")
+# salom_ber("sharifa")
+
+# def kopaytma(son1, son2):
+#     """
+#     Ikkita soni ko'paytma ko'rinishda taqdim qiladigan function
+#     """
+# print(f"{son1} * {son2}= {son1*son2}")
+# kopaytma(9,74)
+
+# Ixtiyori sonning 3-darajasini hisoblovchi PowerA3 nomli funksiya hosil qiling PowerA3 funksiyasi orqali A,B,C haqiqiy sonlarining va D,E butun sonlarining 3-darajasini hisoblovchi programma tuzing
+# def PowerA3(son)
+# """
+# sonning 3-darajasini hisoblovchi function
+# """
+# daraja=son**3
+
+# print(f"{son}ning 3-darajasi {daraja} ga teng")
+# PowerA3(5)
+# PowerA3(2)
+
+
